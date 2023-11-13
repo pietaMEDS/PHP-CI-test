ee2abf6f03827ad0bcac2fec9db5235a01641f8e
Sagynay Utralinova <sagynay.saydildina.1997@mail.ru>
2023-11-13 15:37:49 +0500

    admin panel (Артём идёшь нахуй)

---
+++ panel/admin.php
@@ -0,0 +1,29 @@
+
+    <?php
+    $pageStyles = 'admin.css';
+    require "../Header/Head.php";
+    require "../Header/header.php";
+    ?>
+
+    <body>
+        <search class="search">
+            <div>
+                <input type="text" id="search" placeholder="Поиск">
+            </div>
+        </search>
+        <items>
+            <div>
+                <div class="img">
+                    <img src="#" id="img" alt="image">
+                </div>
+                <div class="name">
+                    <span id="ObjName">Text</span>
+                </div>
+                <div class="price">
+                    <input type="number" id="price" value="01">
+                    <span id="Obj">%</span>
+                </div>
+</div>
+        </items>
+    </body>
+</html>

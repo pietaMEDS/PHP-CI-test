1fe5d89480d931cdfda67e3640d8bdd2e34274f9
pietaMEDS <maxibanp@gmail.com>
2023-11-19 20:09:03 +0500

    [Server] fix

--- admin/redactBaza.php
+++ admin/redactBaza.php
@@ -1,26 +1,25 @@
 <?php 
 session_start();
+$pageStyles = 'admin.css';
+require "../Header/Head.php";
+require "../Header/Header.php";
 require_once '../admin/connekt.php';
 
 if(!empty($_SESSION["login"])){
     echo "Добро пожаловать ".$_SESSION['login'];
 } else {
-    echo 'ГУЛЯЙ ВАСЯ';
+    echo 'Unknow';
 }
-
-$sql = $mysqli->prepare("SELECT * FROM const");
-$sql->execute();
-$result = $sql->get_result();
-$res = $result->fetch_assoc();
-$sql->close();
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST["id"];
     $name = $_POST["name"];
     $count = $_POST["count"];
     
-    $row = "UPDATE const SET name=?, count=?";
+    // echo $id;
+    
+    $row = "UPDATE const SET name = ?, count= ? WHERE id= ?";
     $query = $mysqli->prepare($row);
-    $query->bind_param("si", $name, $count);
+    $query->bind_param("sii", $name, $count, $id);
     $query->execute();
     $query->close();
 }
@@ -36,10 +35,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
     <h1>редактировать базу данных</h1>
     <a href="../index.php">Выйти</a>
-    <form action="../admin/redactBaza.php" method="POST">
-        <input type="text" name='name' value="<?php echo $res['name'] ?>">
-        <input type="text" name='count' value="<?php echo $res['count'] ?>">
+    <items>
+        <?php
+    $name = mysqli_query($mysqli, "SELECT * FROM const");
+    while($row_rs = mysqli_fetch_assoc($name)) // массив с данными
+        {
+        ?>
+        <?php
+            // foreach($row_rs as $val) //перебор массива в цикле
+            // {
+                echo '
+                <form action="../admin/redactBaza.php" method="POST">
+                <item>
+            <div class="img">
+                <input name="id" readonly value="'.$row_rs["id"].'">
+            </div>
+            <div class="name">
+                <input name="name" id="ObjName" value="'.$row_rs["name"].'">
+            </div>
+            <div class="price">
+                <input name="count" type="number" id="price" value="'.$row_rs["count"].'">
+                <span id="Obj">Руб</span>
+            </div>
+        </item>
         <button type="submit">Сохранить</button>
-    </form>
+        </form>';
+        }?>
+    </items>
 </body>
 </html>

5cd7238bb1ff00bb2d53d901236fabe194afdb7a
razeXrite <sobaka9631@gmail.com>
2023-11-22 23:31:02 +0500

    finalkaVerstka

--- Header/Head.php
+++ Header/Head.php
@@ -10,5 +10,5 @@
     <link rel="icon" href="./../assets/icon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;1,300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,400;1,300&display=swap" rel="stylesheet">
 </head>
--- finalSmeta/finalSmeta.php
+++ finalSmeta/finalSmeta.php
@@ -8,15 +8,22 @@ require './../Header/Header.php';
 
     <h1 id="firstH">Предварительная смета</h1>
     
-    <div class="allElements">
-        <p>Фундамент</p>
-        <span class="mainMat">Винтовые сваи</span><span class="mainPrice">Цена руб.</span>
-        <ul>
-            <li>материал <span>цена</span></li>
-            <li>материал <span>цена</span></li>
-            <li>материал <span>цена</span></li>
-        </ul>
+    <div class="gridContainer">
+        <h2 class="gridZag">Выбранные эллементы</h2>
+        <h2 class="gridZag">Обязательные эллементы</h2>
+        <div class="elementContainer">
+            <span class="majorType">Фундамент</span><br>
+            <span class="minorType">Винтовая свая</span>
+            <div class="priceNBtn">
+                <span class="majorPrice">170 000 руб.</span>
+                <button class="opener"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+</svg></button>
+            </div>
+        </div>
     </div>
-    
+    <script>
+        
+    </script>
 </body>
 

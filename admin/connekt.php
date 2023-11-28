bd2fb6f786804f568536e4b54f38987fdc45a8bc
razeXrite <sobaka9631@gmail.com>
2023-11-28 20:20:57 +0500

    zakrivashka v 5 strok AHUET

--- admin/connekt.php
+++ admin/connekt.php
@@ -1,8 +1,8 @@
 <?php
   $db_host = 'localhost';
   $db_user = 'root';
-  $db_password = '';
-  $db_db = 'testing';
+  $db_password = 'root';
+  $db_db = 'bd';
  
   $mysqli = @new mysqli(
     $db_host,
